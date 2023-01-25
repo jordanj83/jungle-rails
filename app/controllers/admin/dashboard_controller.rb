da4e84b53b3e014e9165b88558d4b9c92590b277
jordanj83 <jordan.johnson@telus.com>
2023-01-25 15:27:03 -0800

    added categories and products to the admin page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,9 +1,10 @@
 class Admin::DashboardController < ApplicationController
 
-  http_basic_authenticate_with name: Rails.configuration.admin[:user],
-  password: Rails.configuration.admin[:password]
+  http_basic_authenticate_with name: Rails.configuration.admin[:user], password: Rails.configuration.admin[:password]
 
 
   def show
+    @products = Product.all
+    @category = Category.all
   end
 end
